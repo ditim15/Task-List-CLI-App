7b259bffb1b8a1b23901b19ba43170729a288c4e
ditim15 <ditim456@gmail.com>
2026-06-01 18:50:23 -0400

    Added read_tasks method

--- task_manager/storage.py
+++ task_manager/storage.py
@@ -1,2 +1,11 @@
 import json
 from task_manager.task import Task
+
+FILE = "tasks.json"
+
+def read_tasks():
+    try:
+        with open(FILE, "r") as f:
+            return [Task.from_dict(task) for task in json.load(f)]
+    except FileNotFoundError:
+        return []
